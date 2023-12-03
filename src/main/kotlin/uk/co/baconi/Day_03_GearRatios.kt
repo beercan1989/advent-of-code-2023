ba730869abc1d3f1529e7317552b52c9c98f6a96
James Bacon <james@baconi.co.uk>
2023-12-03 14:31:00 +0000

    Day Three - Part 2

--- src/main/kotlin/uk/co/baconi/Day_03_GearRatios.kt
+++ src/main/kotlin/uk/co/baconi/Day_03_GearRatios.kt
@@ -1,5 +1,6 @@
 package uk.co.baconi
 
+// https://adventofcode.com/2023/day/3
 fun main() {
     dayThreePartOne()
     dayThreePartTwo()
@@ -44,10 +45,19 @@ fun dayThreePartTwo() {
     println()
     println("--- Part Two --- ")
 
-    val exampleAnswer = ""
+    val exampleAnswer = dayThreeExample
+        .extractAsteriskIndices()
+        .findGearRatios(dayThreeExample)
+        .sum()
+
     println("Example answer: $exampleAnswer")
 
-    val answer = ""
+    val engineSchematic = getEngineSchematic()
+    val answer = engineSchematic
+        .extractAsteriskIndices()
+        .findGearRatios(engineSchematic)
+        .sum()
+
     println("Answer: $answer")
 }
 
@@ -55,15 +65,17 @@ fun getEngineSchematic(): List<String> = AdventOfCode2023.readResourceByLines("/
 
 data class NumberWithPosition(val number: String, val start: Int, val end: Int)
 
-fun List<String>.extractNumbers(): List<List<NumberWithPosition>> = map { line ->
+fun List<String>.extractNumbers(): List<List<NumberWithPosition>> = map(String::extractNumbers)
+
+fun String.extractNumbers(): List<NumberWithPosition> {
 
     val result = mutableListOf<NumberWithPosition>()
 
-    line.foldIndexed("") { index, accumulated, current ->
-        if(current.isDigit() && index < line.length - 1) return@foldIndexed accumulated + current
-        if(accumulated.isEmpty()) return@foldIndexed ""
+    foldIndexed("") { index, accumulated, current ->
+        if (current.isDigit() && index < length - 1) return@foldIndexed accumulated + current
+        if (accumulated.isEmpty()) return@foldIndexed ""
 
-        if(current.isDigit()) {
+        if (current.isDigit()) {
             val updated = accumulated + current
             result.add(NumberWithPosition(updated, (index + 1) - updated.length, index + 1))
         } else {
@@ -73,7 +85,11 @@ fun List<String>.extractNumbers(): List<List<NumberWithPosition>> = map { line -
         return@foldIndexed ""
     }
 
-    result
+    return result
+}
+
+fun List<String>.extractAsteriskIndices(): List<List<Int>> = map { line ->
+    line.mapIndexedNotNull { index, current -> if (current == '*') index else null }
 }
 
 fun List<List<NumberWithPosition>>.findParts(source: List<String>): List<Int> = flatMapIndexed { lineNumber, numbers ->
@@ -100,4 +116,34 @@ fun String.hasSymbol(start: Int, end: Int): Boolean {
     val from = (if (start <= 0) start else start - 1)
     val to = (if (end + 1 < length) end + 1 else end)
     return substring(from..<to).contains(Regex("[^0-9.]"))
+}
+
+fun List<List<Int>>.findGearRatios(source: List<String>): List<Int> = flatMapIndexed { lineNumber, asteriskIndices ->
+
+    val lineAbove = (if (lineNumber == 0) null else source[lineNumber - 1]).orEmpty().extractNumbers()
+    val lineCurrent = source[lineNumber].extractNumbers()
+    val lineBelow = (if (lineNumber + 1 < source.size) source[lineNumber + 1] else null).orEmpty().extractNumbers()
+
+    asteriskIndices.mapNotNull { index ->
+
+        val above = lineAbove.getGearRatioParts(index)
+        val current = lineCurrent.getGearRatioParts(index)
+        val below = lineBelow.getGearRatioParts(index)
+
+        val parts = above + current + below
+
+        if (parts.isEmpty() || parts.size < 2 || parts.size >= 3) {
+            null
+        } else {
+            val (first, second) = parts
+            first * second
+        }
+    }
+}
+
+fun List<NumberWithPosition>.getGearRatioParts(index: Int): List<Int> = mapNotNull { number ->
+    if ((index - 1) in number.start..<number.end) number.number.toInt()
+    else if (index in number.start..<number.end) number.number.toInt()
+    else if ((index + 1) in number.start..<number.end) number.number.toInt()
+    else null
 }
