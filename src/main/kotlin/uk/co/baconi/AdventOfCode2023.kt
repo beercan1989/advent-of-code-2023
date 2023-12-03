a08def4bffb20d3b38dfb14d5e19379256926cf2
James Bacon <james@baconi.co.uk>
2023-12-03 14:55:08 +0000

    Now with improved resource file reading

--- src/main/kotlin/uk/co/baconi/AdventOfCode2023.kt
+++ src/main/kotlin/uk/co/baconi/AdventOfCode2023.kt
@@ -1,12 +1,8 @@
 package uk.co.baconi
 
-import java.io.File
-
 object AdventOfCode2023 {
 
     fun readResourceByLines(path: String): List<String> {
-        return checkNotNull(this::class.java.getResource(path)?.toURI()?.let(::File)?.readLines()) {
-            "Resource was not found at: $path"
-        }
+        return checkNotNull(this::class.java.getResourceAsStream(path)).reader().readLines()
     }
 }
